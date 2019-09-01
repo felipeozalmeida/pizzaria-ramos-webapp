a09a1216fd643824629f2daff7421e3a80fd6406
Felipe Almeida <felipeortalmeida@gmail.com>
2019-09-01 17:19:09 -0300

    Updated mock to get random images

--- src/services/pizza.service.js
+++ src/services/pizza.service.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 function get() {
-  return Axios.get("http://www.mocky.io/v2/5d6aecda3100000e00d20785");
+  return Axios.get("http://www.mocky.io/v2/5d6c26b43300006200346d38");
 }
 
 export default {
