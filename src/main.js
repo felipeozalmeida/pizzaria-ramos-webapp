693c76df788495f935fb766e1dd0af91434596e2
Felipe Almeida <felipeortalmeida@gmail.com>
2019-09-01 14:04:20 -0300

    Added vue-fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./plugins/bootstrap-vue";
+import "./plugins/vue-fontawesome";
 
 Vue.config.productionTip = false;
 
---
+++ src/plugins/vue-fontawesome.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faCircleNotch);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
