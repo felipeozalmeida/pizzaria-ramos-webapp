ac337e8a62226f604a453278cd029bfa878fa640
Felipe Almeida <felipeortalmeida@gmail.com>
2019-09-08 12:51:05 -0300

    Added pizza creation mocks

--- src/services/pizza.service.js
+++ src/services/pizza.service.js
@@ -4,6 +4,13 @@ function get() {
   return Axios.get("http://www.mocky.io/v2/5d6c26b43300006200346d38");
 }
 
+function create() {
+  return Axios.post("http://www.mocky.io/v2/5d751d0f310000d82b95059a"); // 201
+  // return Axios.post("http://www.mocky.io/v2/5d75226d310000a61e9505a1"); // 409
+  // return Axios.post("http://www.mocky.io/v2/5d7522bf310000b4229505a3"); // 500
+}
+
 export default {
-  get
+  get,
+  create
 };
