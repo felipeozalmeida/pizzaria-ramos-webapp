dd440911b4f1b72c354d21ca79eeb5045b9141e1
Felipe Almeida <felipeortalmeida@gmail.com>
2019-09-14 19:15:03 -0300

    Created new function to generate FormData object

--- src/services/pizza.service.js
+++ src/services/pizza.service.js
@@ -1,17 +1,21 @@
 import Axios from "axios";
 
+function convertToFormData(pizza) {
+  const formData = new FormData();
+  if (pizza.imageFile) formData.append("imageFile", pizza.imageFile);
+  formData.append("formData", JSON.stringify(pizza));
+  return formData;
+}
+
 function get() {
   return Axios.get("http://www.mocky.io/v2/5d6c26b43300006200346d38"); // 200
   // return Axios.get("http://www.mocky.io/v2/5d7522bf310000b4229505a3"); // 500
 }
 
 function create(pizza) {
-  const formData = new FormData();
-  if (pizza.imageFile) formData.append("imageFile", pizza.imageFile);
-  formData.append("formData", JSON.stringify(pizza));
   return Axios.post(
     "http://www.mocky.io/v2/5d751d0f310000d82b95059a",
-    formData
+    convertToFormData(pizza)
   ); // 201
   // return Axios.post("http://www.mocky.io/v2/5d75226d310000a61e9505a1"); // 409
   // return Axios.post("http://www.mocky.io/v2/5d7522bf310000b4229505a3"); // 500
